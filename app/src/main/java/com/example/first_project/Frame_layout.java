e24a0bc7aa653ef2fb10b2d4c1e456239e606487
Sairaj <saibudhathoki710@gmail.com>
2021-07-25 14:43:27 +0545

    First Assignment-Table and frame layout

---
+++ app/src/main/java/com/example/first_project/Frame_layout.java
@@ -0,0 +1,14 @@
+package com.example.first_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frame_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+}
---
+++ app/src/main/java/com/example/first_project/Table_layout.java
@@ -0,0 +1,14 @@
+package com.example.first_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Table_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout);
+    }
+}
